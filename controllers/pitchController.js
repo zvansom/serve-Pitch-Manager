67c14b320a6a8142b63ca797bfced175e9ecc16e
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-28 21:53:07 -0700

    Initial Pitch Form controller created.

--- controllers/pitchController.js
+++ controllers/pitchController.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
+const Pitch = mongoose.model('Pitch');
+
 exports.homePage = (req, res) => {
   res.render('index');
 }
 
 exports.addPitch = (req, res) => {
   res.render('editPitch', { title: 'Add a Pitch' })
+}
+
+exports.createPitch = async (req, res) => {
+  const pitch = await (new Pitch(req.body)).save();
+  req.flash('success', `Created ${pitch.title}.`);
+  res.redirect(`/store/${pitch.slug}`);
 }
