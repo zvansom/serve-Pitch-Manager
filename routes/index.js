1a4783e5c9ba4c2e4a5c337fb1bf15785a7add7a
Zach VanSomeren <zvansomeren@gmail.com>
2018-09-29 09:43:44 -0700

    Route, controller, and view for login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pitchController = require('../controllers/pitchController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
@@ -14,5 +15,7 @@ router.get('/pitch/:slug', catchErrors(pitchController.getPitchBySlug) )
 router.get('/add', pitchController.addPitch);
 router.post('/add', catchErrors(pitchController.createPitch));
 router.post('/add/:id', catchErrors(pitchController.updatePitch));
+
+router.get('/login', userController.loginForm);
 
 module.exports = router;
