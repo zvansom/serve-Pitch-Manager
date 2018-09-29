a4e9f7be1e5dd5cae6f5231aff3c793b4d34c82a
Zach VanSomeren <zvansomeren@gmail.com>
2018-09-29 13:00:01 -0700

    Register post route created. Validates, registers and logs in new user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pitchController = require('../controllers/pitchController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
@@ -18,5 +19,15 @@ router.post('/add/:id', catchErrors(pitchController.updatePitch));
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
+
+// TODO for register:
+// 1. Validate the registration data
+// 2. Register the user
+// 3. Log the user in
+router.post('/register', 
+  userController.validateRegister,
+  userController.register,
+  authController.login,
+);
 
 module.exports = router;
