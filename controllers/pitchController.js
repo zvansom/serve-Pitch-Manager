2c24ca978d0da3e8ab89fbbe6074932246a0c9c8
Zach VanSomeren <zvansomeren@gmail.com>
2018-09-29 08:40:21 -0700

    Edit controller and update controller created

--- controllers/pitchController.js
+++ controllers/pitchController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Pitch = mongoose.model('Pitch');
 
-exports.homePage = (req, res) => {
-  res.render('index');
-}
-
 exports.addPitch = (req, res) => {
   res.render('editPitch', { title: 'Add a Pitch' })
 }
@@ -17,6 +13,21 @@ exports.createPitch = async (req, res) => {
 
 exports.getPitches = async (req, res) => {
   const pitches = await Pitch.find()
-  console.log(pitches);
   res.render('pitches', { title: 'Pitches', pitches });
+}
+
+exports.editPitch = async (req, res) => {
+  const pitch = await Pitch.findOne({ _id: req.params.id });
+  // TODO: Confirm user owns pitch
+  res.render('editPitch', { title: `Edit ${pitch.title}`, pitch })
+}
+
+exports.updatePitch = async (req, res) => {
+  const pitch = await Pitch.findOneAndUpdate({ _id: req.params.id }, req.body, 
+    { 
+    new: true,
+    runValidators: true,
+   }).exec();
+   req.flash('success', `Successfully updated <strong>${pitch.title}</strong> <a href="pitches/$pitch.slug}">View Pitch </a> `)
+   res.redirect(`/pitches/${pitch._id}/edit`);
 }
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,12 @@ const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
 router.get('/', catchErrors(pitchController.getPitches));
+
 router.get('/pitches', catchErrors(pitchController.getPitches));
+router.get('/pitches/:id/edit', catchErrors(pitchController.editPitch))
+
 router.get('/add', pitchController.addPitch);
 router.post('/add', catchErrors(pitchController.createPitch));
+router.post('/add/:id', catchErrors(pitchController.updatePitch));
 
 module.exports = router;
