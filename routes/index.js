5f2de8af30bb8c57ac077a50eb5f0bec9649ed50
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-28 21:53:37 -0700

    Post route connected for form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const pitchController = require('../controllers/pitchController');
+const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
 router.get('/', pitchController.homePage);
 router.get('/add', pitchController.addPitch);
+router.post('/add', catchErrors(pitchController.createPitch));
 
 module.exports = router;
