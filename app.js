61b47e5f821e6205a403b0e14a155dc584be1394
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-08 13:11:44 -0700

    Remove passport dependencies.  JWT authorization

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-const express = require('express');
-const session = require('express-session');
-const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo')(session);
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const promisify = require('es6-promisify');
-const flash = require('connect-flash');
-const expressValidator = require('express-validator');
-const routes = require('./routes/index');
-const helpers = require('./helpers');
-const errorHandlers = require('./handlers/errorHandlers');
 const cors = require('cors');
-require('./handlers/passport');
-
-// GraphQL Dependencies
+const express = require('express');
+const expressJWT = require('express-jwt');
+const expressValidator = require('express-validator');
 const graphqlHTTP = require('express-graphql');
+const path = require('path');
+
+const errorHandlers = require('./handlers/errorHandlers');
+const routes = require('./routes/index');
 const schema = require('./schema/schema');
 
-// create our Express app
+// Create our Express app
 const app = express();
 
 // Allow cross-origin
 app.use(cors());
 
+// * Views only used for forgotten password email
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
 app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
@@ -39,40 +31,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
 app.use(expressValidator());
 
-// populates req.cookies with any cookies that came along with the request
-app.use(cookieParser());
-
-// Sessions allow us to store data on visitors from request to request
-// This keeps users logged in and allows us to send flash messages
-app.use(session({
-  secret: process.env.SECRET,
-  key: process.env.KEY,
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}));
-
-// Passport JS is what we use to handle our logins
-app.use(passport.initialize());
-app.use(passport.session());
-
-// The flash middleware let's us use req.flash('error', 'Shit!'), which will then pass that message to the next page the user requests
-app.use(flash());
-
 // pass variables to our templates + all requests
-app.use((req, res, next) => {
-  res.locals.h = helpers;
-  res.locals.flashes = req.flash();
-  res.locals.user = req.user || null;
-  res.locals.currentPath = req.path;
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.h = helpers;
+//   res.locals.user = req.user || null;
+//   next();
+// });
 
-// promisify some callback based APIs
-app.use((req, res, next) => {
-  req.login = promisify(req.login, req);
-  next();
-});
+// Helper function for Auth
+function fromRequest(req) {
+  if (req.body.headers.Authorization && 
+    req.body.headers.Authorization.split(' ')[0] === 'Bearer') {
+      return req.body.headers.Authorization.split(' ')[1];
+  }
+  return null;
+}
+
+// JWT 
+app.use(expressJWT({
+    secret: 'test this secret',
+    getToken: fromRequest,
+  })
+  .unless({
+    path: [
+      { url: '/login', methods: ['POST'] },
+      { url: '/register', methods: ['POST'] },
+      { url: '/account/forgot', methods: ['POST'] },
+      { url: '/account/reset/:token', methods: ['POST'] },
+    ],
+}));
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
